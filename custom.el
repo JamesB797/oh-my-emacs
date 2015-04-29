9f7ffdc065b36873243552bd10f5414068eed8dd
JamesB797 <jamesb797@live.com>
2015-04-29 15:48:17 -0500

    Coloring and convenience function

--- custom.el
+++ custom.el
@@ -124,4 +124,5 @@ inversion of gas-comment-region"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(smerge-refined-added ((t (:inherit smerge-refined-change :background "#114411"))))
+ '(smerge-refined-removed ((t (:inherit smerge-refined-change :background "#441111")))))
